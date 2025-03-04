f1127d840fb3cbd3a1d62357aa071c908ffda3d9
Luke Goodwin <lukegoodwin1477@outlook.com>
2025-03-04 16:23:41 +0000

    handle undefined frontend routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -24,6 +29,7 @@ function App() {
                 </ProtectedRoute>
               }
             />
+            <Route path="*" element={<Navigate to="/home" replace />} />
           </Routes>
         </Router>
       </AuthProvider>
